6fa79edb00662c833c41d1e576d12ff3381e695c
nastya <anrozk@taltech.ee>
2026-05-23 21:06:37 +0300

    updateа

--- stars-roulette/vite.config.js
+++ stars-roulette/vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/stars-roulette/',  // название твоего репозитория на GitHub
+  base: '/test/',  // ← название репо
 })
